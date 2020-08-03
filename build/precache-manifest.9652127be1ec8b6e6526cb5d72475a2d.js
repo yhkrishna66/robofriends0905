d3719b3a7503f3d280ddbe49b79df2d9a9c57385
Hari krishna <hyadlapalli@dminc.com>
2020-08-03 12:47:22 +0530

    Code changes to master branch2h

---
+++ build/precache-manifest.9652127be1ec8b6e6526cb5d72475a2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f66825d3495e9cce08d1cf9d34c69954",
+    "url": "/yhkrishna66/robofriends0905/index.html"
+  },
+  {
+    "revision": "f74532278f317f912e22",
+    "url": "/yhkrishna66/robofriends0905/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d6434ef6148864ab6e0a",
+    "url": "/yhkrishna66/robofriends0905/static/css/main.4d2dc054.chunk.css"
+  },
+  {
+    "revision": "f74532278f317f912e22",
+    "url": "/yhkrishna66/robofriends0905/static/js/2.80f57591.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yhkrishna66/robofriends0905/static/js/2.80f57591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6434ef6148864ab6e0a",
+    "url": "/yhkrishna66/robofriends0905/static/js/main.fadee0e5.chunk.js"
+  },
+  {
+    "revision": "1a8ac621f9e49dd5e257",
+    "url": "/yhkrishna66/robofriends0905/static/js/runtime-main.ff6e0672.js"
+  }
+]);
